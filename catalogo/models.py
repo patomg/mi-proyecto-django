8d11cf8d6de5050856258218ec53685b52d6bb9d
patotapia <pato_gc@outlook.cl>
2026-08-22 01:13:46 -0400

    SCRUM-4: Crear app catalogo con modelo Producto y migraciones iniciales

---
+++ catalogo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
